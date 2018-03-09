a77c2171c0e406c2573c78fd50ed5028ed20ee25
Giovanni Bassi <giggio@giggio.net>
2018-03-08 16:11:54 -0800

    Add project files.

---
+++ Blazor3/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Blazor.Browser.Services;
+using System;
+
+namespace Blazor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.AspNetCore.Blazor.Browser.Interop.RegisteredFunction.Invoke<object>("done", "hello from c#");
+        }
+        public static string Echo(string msg) => $"Echo: {msg}";
+        public static string Fatorial(string nString)
+        {
+            var n = Convert.ToInt32(nString);
+            if (n <= 1) return "-1";
+            var total = 1;
+            while (n > 1)
+            {
+                total *= n;
+                n--;
+            }
+            return total.ToString();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Experimenting with Wasm and .NET with Blazor
+
+This is an experiment where I wanted to use .NET not to render a page but to do
+some computation, while I still use HTML and JavaScript.
+
+I got it working, and you can try it [installing Blazor](http://aka.ms/install-blazor) and running this project.
+
+## Author
+
+* [Giovanni Bassi](http://blog.lambda3.com.br/L3/giovannibassi/), aka Giggio, [Lambda3](http://www.lambda3.com.br), [@giovannibassi](https://twitter.com/giovannibassi)
+
+## Contact
+
+Use Twitter.
