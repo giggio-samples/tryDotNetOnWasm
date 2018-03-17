a3598a838e8519b181000b7b645a4d00a71d4087
Giovanni Bassi <giggio@giggio.net>
2018-03-16 22:38:20 -0300

    Add performance tests

--- Blazor3/Program.cs
+++ Blazor3/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Blazor.Browser.Services;
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Blazor3
 {
@@ -10,17 +13,64 @@ namespace Blazor3
             Microsoft.AspNetCore.Blazor.Browser.Interop.RegisteredFunction.Invoke<object>("done", "hello from c#");
         }
         public static string Echo(string msg) => $"Echo: {msg}";
-        public static string Fatorial(string nString)
+        public static string Fatorial(string nString) => Fatorial(Convert.ToInt32(nString)).ToString();
+        public static int Fatorial(int n)
         {
-            var n = Convert.ToInt32(nString);
-            if (n <= 1) return "-1";
+            if (n <= 1) return -1;
             var total = 1;
             while (n > 1)
             {
                 total *= n;
                 n--;
             }
-            return total.ToString();
+            return total;
+        }
+
+        public static string DesempenhoThreads()
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            Parallel.For(0, 1_000_000, i => Fatorial(12));
+            sw.Stop();
+            return sw.ElapsedMilliseconds.ToString();
+        }
+
+        public static string Desempenho()
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var res = Fatorial(12);
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds.ToString();
+        }
+
+        public static int FatorialNaoOtimizado(int n)
+        {
+            if ((DateTime.Now.Millisecond % 2) == 0)
+            {
+                n++;
+                if (n > 12) n = 12;
+            }
+            if (n <= 1) return -1;
+            var total = 1;
+            while (n > 1)
+            {
+                total *= n;
+                n--;
+            }
+            return total;
+        }
+
+        public static string DesempenhoNaoOtimizado()
+        {
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var res = FatorialNaoOtimizado(12);
+            }
+            sw.Stop();
+            return sw.ElapsedMilliseconds.ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -3,7 +3,22 @@
 This is an experiment where I wanted to use .NET not to render a page but to do
 some computation, while I still use HTML and JavaScript.
 
-I got it working, and you can try it [installing Blazor](http://aka.ms/install-blazor) and running this project.
+I got it working, and you can try it
+[installing Blazor](http://aka.ms/install-blazor) and running this project.
+
+You can call from C# into JS and from JS into C#.
+
+I also ran some performance tests. Blazor is still slower than JS. Here are
+the results:
+
+| Performance   | Chrome | Firefox |
+| ------------- | ------:| -------:|
+| C# | 1497 | 926|
+| JS | 20 | 12|
+| C# non optimized | 10.003 | 6.608 |
+| JS non optimized | 845 | 1.020 |
+
+Don't call the threads example, it locks the browser.
 
 ## Author
 
